d58ce4f79d8e8a37a96d91ab6ff9f81017dce188
vietdm <vietdau33@gmail.com>
2023-12-20 19:54:16 +0700

    Copy content

--- src/components/pages/Index/Game/Detail.tsx
+++ src/components/pages/Index/Game/Detail.tsx
@@ -1,11 +1,13 @@
+import ContentCopyRoundedIcon from "@mui/icons-material/ContentCopyRounded";
 import { Stack } from "@mui/material";
 import { useMemo } from "react";
 
 import { TGameDetailData, TStartGame } from "@/@types/game";
 import { SuccessButton } from "@/components/ui/Button";
 import { Table } from "@/components/ui/Table";
+import { useToast } from "@/hooks/useToast";
 import { useUser } from "@/hooks/useUser";
-import { uuidv4 } from "@/utils/helper";
+import { copyContent, uuidv4 } from "@/utils/helper";
 
 type Props = {
   headers: Array<string>;
@@ -16,10 +18,17 @@ type Props = {
 
 export const GameDetail = ({ headers, data, startGame, group }: Props) => {
   const { user } = useUser();
+  const toast = useToast();
 
   const nickname = useMemo(() => {
     return user?.nickname ?? "nickname";
   }, [user]);
+
+  const triggerCopyContent = (content: string) => {
+    copyContent(content, () => {
+      toast.success("Copy nội dung thành công!", { autoClose: 2000 });
+    });
+  };
 
   return (
     <Table headers={headers}>
@@ -27,9 +36,18 @@ export const GameDetail = ({ headers, data, startGame, group }: Props) => {
         return (
           <tr key={"tr-game-" + uuidv4()}>
             <td className="py-[8px] px-[4px] border border-[#ddd] text-center min-w-[100px]">
-              <b className="text-[#6d2f0f]">
-                {nickname} {key}
-              </b>
+              <Stack
+                direction="row"
+                alignItems="center"
+                justifyContent="center"
+                className="cursor-pointer select-none"
+                onClick={() => triggerCopyContent(`${nickname} ${key}`)}
+              >
+                <b className="text-[#6d2f0f]">
+                  {nickname} {key}
+                </b>
+                <ContentCopyRoundedIcon className="!text-[18px] text-[#ff55a5] ml-1" />
+              </Stack>
             </td>
             <td className="py-[8px] px-[4px] border border-[#ddd] min-w-[150px]">
               <Stack
